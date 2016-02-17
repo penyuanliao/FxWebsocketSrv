1ae4682457ac3a00ae12b25241e542f64ab07022
penyuanliao <anaylst@gmail.com>
2016-02-17 08:23:35 +0800

    move args to config file

--- NodeLiveStream.sh
+++ NodeLiveStream.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
    echo '    ______    '
    echo '   / ____/ __ '
@@ -7,22 +7,30 @@
    echo '/_/   /_/\_\  '  
    echo '              '
 
-SERV_PARAMS="--max-old-space-size=8192 --expose-gc --always-compact"
+configfile="./configfile/config.cfg";
 
-SERV_FILE="FxLiveStreamSrv.js"
+if [ -f "$configfile" ]
+then
+   source "$configfile"
+else
+   echo '### No such file or directory... ###'
+   exit 0
+fi
 
-SERV_MULTI_FILE="FxLiveMaster.js"
+#SERV_PARAMS="--max-old-space-size=8192 --expose-gc --always-compact"
+#
+#SERV_FILE="../FxLiveStreamSrv.js"
+#
+#SERV_MULTI_FILE="../FxLiveMaster.js"
+#
+#SERV_PORT="3000"
 
-SERV_PORT="80"
+#SERV_PATH="rtmp://183.182.79.162:1935/video/daabb/video0/ rtmp://183.182.79.162:1935/video/daabc/video0/"
+echo "args:" + node ${SERV_PARAMS} ${SERV_MULTI_FILE} -p ${SERV_PORT} -f "rtmp://183.182.79.162:1935/video/daabb/video0/ rtmp://183.182.79.162:1935/video/daabc/video0/"
 
-SERV_PATH='"rtmp://192.168.188.72/video/daabb/video0 rtmp://192.168.188.72/video/daabc/video0 rtmp://192.168.188.72/video/daabd/video0 rtmp://192.168.188.72/video/daabg/video0 rtmp://192.168.188.72/video/daabh/video0 rtmp://192.168.188.72/video/daabdg/video0 rtmp://192.168.188.72/video/daabdh/video0"'
-echo "args:" + node $SERV_PARAMS $SERV_FILE -p $SERV_PORT -f "$SERV_PATH > app.log"
-
-echo exec node ${SERV_PARAMS} ${SERV_MULTI_FILE} -p ${SERV_PORT} -f "rtmp://192.168.188.72/video/daabb/video0 rtmp://192.168.188.72/video/daabc/video0 rtmp://192.168.188.72/video/daabd/video0 rtmp://192.168.188.72/video/daabg/video0 rtmp://192.168.188.72/video/daabh/video0 rtmp://192.168.188.72/video/daabdg/video0 rtmp://192.168.188.72/video/daabdh/video0" > app.log 2>&1 &
-
-exec node ${SERV_PARAMS} ${SERV_MULTI_FILE} -p ${SERV_PORT} -f "rtmp://192.168.188.72/video/daabb/video0/ rtmp://192.168.188.72/video/daabc/video0/ rtmp://192.168.188.72/video/daabd/video0/ rtmp://192.168.188.72/video/daabg/video0/ rtmp://192.168.188.72/video/daabh/video0/ rtmp://192.168.188.72/video/daabdg/video0/ rtmp://192.168.188.72/video/daabdh/video0/" > app.log 2>&1 &
+# exec node ${SERV_PARAMS} ${SERV_MULTI_FILE} -p ${SERV_PORT} -f "rtmp://192.168.188.72/video/daabb/video0/ rtmp://192.168.188.72/video/daabc/video0/ rtmp://192.168.188.72/video/daabd/video0/ rtmp://192.168.188.72/video/daabg/video0/ rtmp://192.168.188.72/video/daabh/video0/ rtmp://192.168.188.72/video/daabdg/video0/ rtmp://192.168.188.72/video/daabdh/video0/" > app.log 2>&1 &
 # exec node ${SERV_PARAMS} ${SERV_FILE} -p ${SERV_PORT} -f "rtmp://192.168.188.72/video/daabb/video0/ rtmp://192.168.188.72/video/daabc/video0 rtmp://192.168.188.72/video/daabd/video0/ rtmp://192.168.188.72/video/daabg/video0/ rtmp://192.168.188.72/video/daabh/video0/ rtmp://192.168.188.72/video/daabdg/video0/ rtmp://192.168.188.72/video/daabdh/video0/ rtmp://192.168.188.72/video/daabb/videosd/ rtmp://192.168.188.72/video/daabc/videosd/ rtmp://192.168.188.72/video/daabd/videosd/ rtmp://192.168.188.72/video/daabg/videosd/ rtmp://192.168.188.72/video/daabh/videosd/ rtmp://192.168.188.72/video/daabdg/videosd/ rtmp://192.168.188.72/video/daabdh/videosd/ rtmp://192.168.188.72/video/daabb/videohd/ rtmp://192.168.188.72/video/daabc/videohd/ rtmp://192.168.188.72/video/daabd/videohd/ rtmp://192.168.188.72/video/daabg/videohd/ rtmp://192.168.188.72/video/daabh/videohd/ rtmp://192.168.188.72/video/daabdg/videohd/ rtmp://192.168.188.72/video/daabdh/videohd/" > app.log 2>&1 &
-
+exec node ${SERV_PARAMS} ${SERV_MULTI_FILE} -p ${SERV_PORT} -f "$SERV_PATH" > /dev/null 2>&1 &
 echo "NodeJS Server START."
 
 
