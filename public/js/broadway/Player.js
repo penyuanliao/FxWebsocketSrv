438568351c04acd30994a0480f8f5e54881004b7
penyuanliao <anaylst@gmail.com>
2016-04-21 17:28:45 +0800

    fix IE

--- public/js/broadway/Player.js
+++ public/js/broadway/Player.js
@@ -26,12 +26,12 @@
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define(["./Decoder", "./YUVCanvas"], factory);
+    define(["./Decoder", "./YUVWebGLCanvas"], factory);
   } else if (typeof exports === 'object') {
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
     // like Node.
-    module.exports = factory(require("./Decoder"), require("./YUVCanvas"));
+    module.exports = factory(require("./Decoder"), require("./YUVWebGLCanvas"));
   } else {
     // Browser globals (root is window)
     root.Player = factory(root.Decoder, root.YUVCanvas);
